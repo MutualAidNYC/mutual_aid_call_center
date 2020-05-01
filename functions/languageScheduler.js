ddf10e443cd9457e3cdf3f6482af44c3b03d76a8
Aaron Young <brainomite@gmail.com>
2020-05-01 14:44:33 -0400

    feat refactor docs: Airtable Schedule integration
    1. Added the ability to pull a json file from a github repository
       * this file is generated from a airtable base and uploaded to github
         via integromat
    2. Refactor common code between scheduler and languageScheduler into a
       library of helper functions that is common between the two
    3. Update package.json
       1. Allow for the axios NPM package
       2. Update the `npm run deploy` script to allow for missing
          .twilio-functions file

--- functions/languageScheduler.js
+++ functions/languageScheduler.js
@@ -1,6 +1,10 @@
-const Moment = require("moment-timezone");
-const MomentRange = require("moment-range");
-const moment = MomentRange.extendMoment(Moment);
+"use strict";
+// find the serverless function path then require the path
+const helpers = require(Runtime.getFunctions()["helperFunctions"].path);
+
+// lets set constants
+const OPEN = "Open";
+const CLOSE = "Close";
 
 /**
  * Main invoked function (invoked by twillio flow)
@@ -13,86 +17,24 @@ const moment = MomentRange.extendMoment(Moment);
  * @return {void}
  */
 exports.handler = function (_context, event, callback) {
-  let response = createResponseObject(); //create Twilio Response
-  const schedule = getPrivateAsset("schedule.js"); //get the schedule from assets
-
+  //create Twilio Response
+  let response = helpers.createResponseObject({
+    isLanguageInOperation: false,
+  });
   // set some variables
   const { language, timezone } = event;
-  const dayOfWeek = moment().tz(timezone).format("dddd");
-  const { begin, end } = schedule.languages[language][dayOfWeek];
-  const inTimeRange = checkIfInRange(begin, end, timezone);
+  const dayOfWeek = helpers.getDayOfWeek(timezone);
+  const schedule = JSON.parse(event.schedule);
+  const daySchedule = schedule[dayOfWeek];
+  const openString = `${language} ${OPEN}`;
+  const closeString = `${language} ${CLOSE}`;
 
-  if (inTimeRange) response.body.isLanguageInOperation = true; // defaults to false
+  const openTime = helpers.getTimeStringFromObject(daySchedule, openString);
+  const closeTime = helpers.getTimeStringFromObject(daySchedule, closeString);
+
+  const inTimeRange = helpers.checkIfInRange(openTime, closeTime, timezone);
+
+  response.body.isLanguageInOperation = inTimeRange;
 
   callback(null, response);
-};
-
-//helper functions
-
-/**
- * Detects if current time is between two time values in a specified timezone
- * This function was obtained from:
- * https://www.twilio.com/blog/advanced-schedules-studio
- * @author Lehel Gyeresi
- * @param {String} begin - The start time in format of "HH:mm:ss"
- * @param {String} end - The end time in format of "HH:mm:ss"
- * @param {String} timezone - A moments (dependancy) accepted timezone
- * @return {Boolean} - True if within the time rande otherwise false
- */
-function checkIfInRange(begin, end, timezone) {
-  const currentDate = moment().tz(timezone).format("MM/DD/YYYY");
-  const now = moment().tz(timezone);
-
-  const beginMomentObject = moment.tz(
-    `${currentDate} ${begin}`,
-    "MM/DD/YYYY HH:mm:ss",
-    timezone
-  );
-  const endMomentObject = moment.tz(
-    `${currentDate} ${end}`,
-    "MM/DD/YYYY HH:mm:ss",
-    timezone
-  );
-  const range = moment.range(beginMomentObject, endMomentObject);
-
-  return now.within(range);
-}
-
-/**
- * Creates an initial Twilio response object with appropriate body defaults
- * @author Aaron Young <hi@aaronyoung.io>
- * @return {Response} - Returns a constructed Twilio response object
- */
-const createResponseObject = () => {
-  /**
-   * create Twilio Response
-   * https://www.twilio.com/docs/runtime/functions/invocation#constructing-a-response
-   */
-
-  let response = new Twilio.Response();
-  response.appendHeader("Access-Control-Allow-Origin", "*");
-  response.appendHeader("Access-Control-Allow-Methods", "OPTIONS POST");
-  response.appendHeader("Content-Type", "application/json");
-  response.appendHeader("Access-Control-Allow-Headers", "Content-Type");
-
-  //create default response body
-  response.body = {
-    isLanguageInOperation: false,
-  };
-
-  return response;
-};
-
-/**
- * Gets and return an exported asset
- * @author Aaron Young <hi@aaronyoung.io>
- * @param {String} assetname - The name of the asset (leave out 'private')
- * @param {Array} data - The array of cell header names
- * @return {Object} - Returns the fetched asset as an Object
- */
-const getPrivateAsset = (assetName) => {
-  const assets = Runtime.getAssets();
-  const privateAsset = assets[`/${assetName}`];
-  const privatePath = privateAsset.path;
-  return require(privatePath);
 };
