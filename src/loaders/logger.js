a510efdc58e75691094052c4ccd2874031f62341
Aaron Young <brainomite@gmail.com>
2020-07-26 13:03:10 -0400

    logs: Add logging for troubleshooting
    1. Add logging for #57 to determine if the issue lies with Airtable or
    us
    2. Add logging for agent-gather for #58

--- src/loaders/logger.js
+++ src/loaders/logger.js
@@ -1,8 +1,7 @@
 const pino = require('pino');
 const expressPino = require('express-pino-logger');
-const config = require('../config');
 
-const useLevel = config.isProduction ? 'info' : 'debug';
+const useLevel = 'trace';
 const logger = pino({ level: process.env.LOG_LEVEL || 'info' });
 const expressLogger = expressPino({ logger, useLevel });
 
