14d42ad0bf0c17edf4ee71571ee89911c0e876a0
D. LoBraico <d@lobraico.com>
2020-10-25 21:37:45 -0400

    add shiftWarnings job

---
+++ src/jobs/shiftWarnings.js
@@ -0,0 +1,15 @@
+const { logger } = require('../loaders/logger');
+// const { setTwilioInfoToApp } = require('../api/routes/sms-incoming');
+const taskRouter = require('../service/twilioTaskRouter');
+const initAirtable = require('../loaders/airtableController');
+
+const runJob = async () => {
+  initAirtable();
+  logger.info('Airtable ready');
+  await taskRouter.init();
+  logger.info('TaskRouter Initialized');
+  logger.info("Sending warnings for tomorrow's shifts");
+  await taskRouter.sendAllShiftWarnings(['2PM - 5PM', '5PM - 8PM']);
+};
+
+runJob();
